3ae996b66ae47d5bc0ac4e6d476b5e5917519dcc
Lyndon Sanche <lsanche@lyndeno.ca>
2020-06-29 20:36:57 -0600

    Increased modularity and improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,27 +15,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {}", e);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return  Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
